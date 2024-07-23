4e990c105f6e60b79b3ed87c54f33e3f8a0d9f67
MCB <mertcagdasbaykal@gmail.com>
2024-07-23 11:53:31 +0300

    Elephant added, keys added

---
+++ Template-main/Assets/Elephant/Core/ElephantThirdPartyIds.cs
@@ -0,0 +1,12 @@
+namespace ElephantSDK
+{
+    public class ElephantThirdPartyIds
+    {
+        public static string FacebookClientToken = "ea1f2334eb872f84e8695c8a01ac23cc";
+        public static string FacebookAppId = "455806204012303";
+        public static string GameId = "1eb248b0366f064b5ea718a811a1816363639d1fed3320bada7fc00fe620beb1";
+        public static string GameSecret = "190dfcc37a0afb7433de2a46895bed8cbaccf33b8387f9919f52efbee0e5b2e8";
+        public static string BundleName = "com.Tinkerbox.TableMatch";
+        public static string AdjustAppKey = "9syholgmgpvk";
+    }
+}
